80fd79607d95e132bf31c7fe4123f1d08a152978
Pavel Tereschenko <me@stupidcat.wtf>
2018-01-21 00:24:55 +0300

    Implement Avatar component

---
+++ src/front/components/Avatar/Avatar.js
@@ -0,0 +1,43 @@
+/* @flow */
+
+import { type ComponentType } from 'react'
+
+import styled from 'styled-jss'
+
+/* :: declare var lifecycle: (Object) => <V>(V) => V */
+
+import {
+  compose,
+  withStateHandlers,
+  defaultProps,
+  // $FlowFixMe
+  lifecycle,
+} from 'recompose'
+
+import defaultImage from './default.svg'
+
+export const Avatar: ComponentType<{
+  url: string,
+  alt?: 'Bot avatar',
+  defaultUrl?: string,
+  onError?: Function,
+}> = compose(
+  defaultProps({ defaultUrl: defaultImage }),
+  withStateHandlers(props => ({
+    src: props.url,
+  }), {
+    setUrl: () => (src: string) => ({ src }),
+    onError: (state, { defaultUrl, onError }) => (payload) => {
+      if (onError) onError(payload)
+      return { src: defaultUrl }
+    },
+  }),
+  lifecycle({
+    componentWillReceiveProps({ url, setUrl }) {
+      if (url !== this.props.url) setUrl(url)
+    },
+  }),
+)(styled('img')({
+  margin: 0,
+  padding: 0,
+}))
--- src/front/index.js
+++ src/front/index.js
@@ -1,1 +1,14 @@
 /* @flow */
+
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+  <div>OLOLO</div>,
+  (document.getElementById('root'): any),
+)
+
+if (
+  module.hot &&
+  typeof module.hot.accept === 'function'
+) module.hot.accept()
